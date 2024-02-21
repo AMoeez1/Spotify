e8a00f2d5b8f0816c76212a3092013a33c30d61f
unknown <mujjeequraishi@gmail.com>
2024-02-21 15:49:37 +0500

    Layout fixed eventually

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,9 +3,16 @@ import Sidebar from "./Sidebar"
 
 function Layout({ children }) {
   return (
-    <div className="">
-      {/* <Navbar/> */}
+    <div className="w-full h-screen bg-black">
+      <div className="w-full h-screen grid grid-cols-[min-content_auto] grid-rows-[5fr_1fr] bg-black">
+      {/* <div className="w-[1000px] bg-green-500">Sidebar</div> */}
       <Sidebar/>
+      <div className="bg-red-500">Main Content</div>
+      <div className="col-span-2 bg-yellow-500">Player</div>
+      {/* <div className="col-span-2 bg-yellow-500">Player</div> */}
+    </div>
+      {/* <Navbar/> */}
+      {/* <Sidebar/> */}
         {/* Children */}
         {children}
         {/* Footer */}
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,60 +1,102 @@
-import { FaArrowRight, FaBars, FaBookOpen, FaPlus,} from "react-icons/fa";
-import { TfiHome, TfiSearch } from "react-icons/tfi";
+import { FaArrowRight, FaBars, FaBookOpen, FaPlus } from "react-icons/fa";
+import { GoHomeFill } from "react-icons/go";
+import { TfiSearch } from "react-icons/tfi";
 import { Link } from "react-router-dom";
 import Navbar from './Navbar'
+import { useEffect, useRef, useState } from "react";
 
-function Sidebar() {
+export default function Sidebar() {
+  const Data = [
+    {
+      Title: "Home",
+      icon: <GoHomeFill size={20} className="inline" />,
+    },
+    {
+      Title: "Search",
+      icon: <TfiSearch size={20} className="inline" />,
+    },
+  ];
+
+  const [width, setWidth] = useState(400);
+  const isResized = useRef(false);
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      if (!isResized.current) {
+        return;
+      }
+
+      setWidth((previousWidth) => previousWidth + e.movementX);
+    });
+
+    window.addEventListener("mouseup", () => {
+      isResized.current = false;
+    });
+  }, []);
   return (
     <>
-    <Navbar className=''/>
-      <div className="fixed bg-black left-0 top-0 p-3 h-full min-w-[25rem]">
-        <div className="bg-[#121212] py-3 px-5 rounded-md text-white">
-          <ul className="text-left">
-            <li className="py-3">
-              <Link className="flex gap-x-4 items-center" to="/">
-                <TfiHome size={20} className="inline" /> <span className="text-xl">Home</span>
-              </Link>
-            </li>
-            <li className="py-3 text-[#828282]">
-              <Link className="flex gap-x-4 items-center hover:text-gray-300" to="search">
-                <TfiSearch size={20} className="inline" /> <span className="text-xl ">Search</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="bg-[#121212] items-center mt-2 py-3 px-5 rounded-md text-white">
-          <div className="flex items-center gap-x-2 text-[#828282]">
-            <div className="flex hover:text-gray-50">
-              <FaBookOpen size={25} />
-              <span className="ml-2 text-xl cursor-pointer">Your Library</span>
-            </div>
-            <div className="flex ml-auto cursor-pointer">
-              <FaPlus size={35} className="py-2 hover:border-transparent hover:rounded-full hover:bg-[#333333]"/>
-              <FaArrowRight size={35} className="py-2 hover:border-transparent hover:rounded-full hover:bg-[#333333]"/>
-            </div>
-          </div>
-          <div className="flex mt-5 gap-x-4">
-            <button className="border-[transparent] bg-[#232323] hover:[#292929] rounded-full py-1 px-3 hover:bg-[#333333]">
-              Playlist
-            </button>
-            <button className="border-[transparent] bg-[#232323] hover:[#292929] rounded-full py-1 px-3 hover:bg-[#333333]">
-              Artist
-            </button>
-            <button className="border-[transparent] bg-[#232323] hover:[#292929] rounded-full py-1 px-3 hover:bg-[#333333]">
-              Albums
-            </button>
-          </div>
-          <div className="flex items-center mt-5 text-[#828282]">
-            <TfiSearch size={20} className="hover:text-gray-50 transition-[.7s ease]" />
-            <div className="flex items-center hover:text-gray-50 gap-x-2 ml-auto transition-[.5s]">
-              <span className="text-lg "> Recent</span>
-              <FaBars size={25} />
-            </div>
-          </div>
-        </div>
-      </div>
+    <div className="flex bg-green-500">
+      <div style={{ width: `${width / 16}rem` }}>Sidebar</div>
+
+      {/* Handle */}
+      <div
+        className="w-4 cursor-col-resize bg-blue-500"
+        onMouseDown={() => {
+          isResized.current = true;
+        }}
+      />
+    </div>
+
     </>
+    // <div className="grid md:grid-cols-4">
+    //   <div className="">
+    //       <div className="bg-gray-100 py-3 px-5 rounded-md ">
+    //         {Data.map((items, index) => {
+    //             return (
+    //               <ul key={index} className="text-left">
+    //                 <li className="py-3">
+    //                   <Link className="flex gap-x-4 items-center" to="/">
+    //                     {items.icon}{" "}
+    //                     <span className="text-xl">{items.Title}</span>
+    //                   </Link>
+    //                 </li>
+    //               </ul>
+    //             );
+    //         })}
+    //       </div>
+    //       <div className="bg-gray-100 items-center mt-3 py-3 px-5 rounded-md text-white">
+    //       <div className="flex items-center gap-x-2 text-[#828282]">
+    //         <div className="flex hover:text-gray-950">
+    //           <FaBookOpen size={25} />
+    //           <span className="ml-2 text-xl cursor-pointer">Your Library</span>
+    //         </div>
+    //         <div className="flex ml-auto cursor-pointer">
+    //           <FaPlus size={35} className="py-2 hover:border-transparent hover:rounded-full hover:bg-gray-50"/>
+    //           <FaArrowRight size={35} className="py-2 hover:border-transparent hover:rounded-full hover:bg-gray-50"/>
+    //         </div>
+    //       </div>
+    //       <div className="flex mt-5 gap-x-4">
+    //         <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 hover:bg-gray-50">
+    //           Playlist
+    //         </button>
+    //         <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 hover:bg-gray-50">
+    //           Artist
+    //         </button>
+    //         <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 hover:bg-gray-50">
+    //           Albums
+    //         </button>
+    //       </div>
+    //       <div className="flex items-center mt-5 text-[#828282]">
+    //         <TfiSearch size={20} className="cursor-pointer hover:text-gray-950 transition-[.7s ease]" />
+    //         <div className="flex cursor-pointer items-center hover:text-gray-950 gap-x-2 ml-auto transition-[.5s]">
+    //           <span className="text-lg "> Recent</span>
+    //           <FaBars size={25} />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <Navbar/>
+    //   </div>
   );
 }
 
-export default Sidebar;
