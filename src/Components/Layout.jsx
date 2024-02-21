a17c92bb4067927b05ba4e84b744be2fb9f87fd5
unknown <mujjeequraishi@gmail.com>
2024-02-21 20:27:45 +0500

    Sidebar added

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -4,12 +4,10 @@ import Sidebar from "./Sidebar"
 function Layout({ children }) {
   return (
     <div className="w-full h-screen bg-black">
-      <div className="w-full h-screen grid grid-cols-[min-content_auto] grid-rows-[5fr_1fr] bg-black">
-      {/* <div className="w-[1000px] bg-green-500">Sidebar</div> */}
+      <div className="w-full h-screen grid grid-cols-[min-content_auto] grid-rows-[7fr_1fr] bg-black">
       <Sidebar/>
       <div className="bg-red-500">Main Content</div>
       <div className="col-span-2 bg-yellow-500">Player</div>
-      {/* <div className="col-span-2 bg-yellow-500">Player</div> */}
     </div>
       {/* <Navbar/> */}
       {/* <Sidebar/> */}
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -17,7 +17,7 @@ export default function Sidebar() {
     },
   ];
 
-  const [width, setWidth] = useState(400);
+  const [width, setWidth] = useState(350);
   const isResized = useRef(false);
 
   useEffect(() => {
@@ -35,12 +35,59 @@ export default function Sidebar() {
   }, []);
   return (
     <>
-    <div className="flex bg-green-500">
-      <div style={{ width: `${width / 16}rem` }}>Sidebar</div>
+    <div className="flex">
+      <div className="min-w-[280px] max-w-[50rem] p-3" style={{ width: `${width / 16}rem`}}>
+          <div className="bg-[#121212] text-gray-50 py-3 px-5 rounded-md ">
+            {Data.map((items, index) => {
+                return (
+                  <ul key={index} className="text-left">
+                    <li className="py-3">
+                      <Link className="flex gap-x-4 items-center" to="/">
+                        {items.icon}{" "}
+                        <span className="">{items.Title}</span>
+                      </Link>
+                    </li>
+                  </ul>
+                );
+            })}
+          </div>
+          <div className="bg-[#121212] items-center mt-3 py-3 px-5 rounded-md text-gray-50">
+          <div className="flex items-center gap-x-2 ">
+            <div className="flex">
+              <FaBookOpen size={25} />
+              <span className="ml-2  cursor-pointer">Your Library</span>
+            </div>
+            <div className="flex ml-auto cursor-pointer">
+              <FaPlus size={35} className="py-2 "/>
+              <FaArrowRight size={35} className="py-2 "/>
+            </div>
+          </div>
+          <div className="flex mt-5 gap-x-4">
+            <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 ">
+              Playlist
+            </button>
+            <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 ">
+              Artist
+            </button>
+            <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 ">
+              Albums
+            </button>
+          </div>
+          <div className="flex items-center mt-5 text-[#828282]">
+            <TfiSearch size={20} className="cursor-pointer" />
+            <div className="flex cursor-pointer items-center gap-x-2 ml-auto ">
+              <span className=""> Recent</span>
+              <FaBars size={20} />
+            </div>
+          </div>
+        </div>
+        
+
+      </div>
 
       {/* Handle */}
       <div
-        className="w-4 cursor-col-resize bg-blue-500"
+        className="w-2 cursor-col-resize bg-gray-900"
         onMouseDown={() => {
           isResized.current = true;
         }}
@@ -48,55 +95,6 @@ export default function Sidebar() {
     </div>
 
     </>
-    // <div className="grid md:grid-cols-4">
-    //   <div className="">
-    //       <div className="bg-gray-100 py-3 px-5 rounded-md ">
-    //         {Data.map((items, index) => {
-    //             return (
-    //               <ul key={index} className="text-left">
-    //                 <li className="py-3">
-    //                   <Link className="flex gap-x-4 items-center" to="/">
-    //                     {items.icon}{" "}
-    //                     <span className="text-xl">{items.Title}</span>
-    //                   </Link>
-    //                 </li>
-    //               </ul>
-    //             );
-    //         })}
-    //       </div>
-    //       <div className="bg-gray-100 items-center mt-3 py-3 px-5 rounded-md text-white">
-    //       <div className="flex items-center gap-x-2 text-[#828282]">
-    //         <div className="flex hover:text-gray-950">
-    //           <FaBookOpen size={25} />
-    //           <span className="ml-2 text-xl cursor-pointer">Your Library</span>
-    //         </div>
-    //         <div className="flex ml-auto cursor-pointer">
-    //           <FaPlus size={35} className="py-2 hover:border-transparent hover:rounded-full hover:bg-gray-50"/>
-    //           <FaArrowRight size={35} className="py-2 hover:border-transparent hover:rounded-full hover:bg-gray-50"/>
-    //         </div>
-    //       </div>
-    //       <div className="flex mt-5 gap-x-4">
-    //         <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 hover:bg-gray-50">
-    //           Playlist
-    //         </button>
-    //         <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 hover:bg-gray-50">
-    //           Artist
-    //         </button>
-    //         <button className="border-[transparent] bg-white text-black rounded-full py-1 px-3 hover:bg-gray-50">
-    //           Albums
-    //         </button>
-    //       </div>
-    //       <div className="flex items-center mt-5 text-[#828282]">
-    //         <TfiSearch size={20} className="cursor-pointer hover:text-gray-950 transition-[.7s ease]" />
-    //         <div className="flex cursor-pointer items-center hover:text-gray-950 gap-x-2 ml-auto transition-[.5s]">
-    //           <span className="text-lg "> Recent</span>
-    //           <FaBars size={25} />
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <Navbar/>
-    //   </div>
   );
 }
 
